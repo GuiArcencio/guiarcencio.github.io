54adc04fcac156c73b66c6639f3f41366ff4bec9
Guilherme Arcencio <guiarcencio@hotmail.com>
2023-03-14 20:47:06 +0000

    Sets up under construction page

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,41 @@
 </script>
 
 <template>
-  <div>
-    é o guizis
-    <nuxt-img id="profile" src="/profile.png" width="600" height="600" />
-  </div>
+  <main>
+    <div>
+      <nuxt-img id="profile" src="/profile.png" width="250" height="250" alt="My profile picture." />
+    </div>
+    <div id="main-text">
+      <p>This page is still under construction. Feel free to check out my social network pages in the meantime :)</p>
+    </div>
+  </main>
 </template>
 
-<style>
+<style lang="scss">
+main {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    gap: 2vw;
+
+    #main-text {
+        p {
+            font-family: 'Source Code Pro', monospace;
+            max-width: 40ch;
+            text-align: justify;
+            font-size: large;
+        }
+    }
+}
+
 #profile {
     border-radius: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-width: 3px;
+    border-color: #151515;
+    border-style: solid;
 }
 </style>
